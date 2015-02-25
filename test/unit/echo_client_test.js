4d53c8fc18a7b65ae2bbc11985c0fc390cbd3dc1
Richard Gubby <rgubby@googlemail.com>
2015-02-25 16:27:38 +0000

    Initial Import

---
+++ test/unit/echo_client_test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should'),
+    assert = require('assert'),
+    echo = require('../../index.js'),
+    rewire = require("rewire");
+
+describe("Echo Node Client Test Suite", function(){
+    describe("- Constructor tests", function(){
+    });
+});
