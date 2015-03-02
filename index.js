1248d675d4e73bbde6ccc058e17595789a6f7d61
Richard Gubby <rgubby@googlemail.com>
2015-03-02 11:39:07 +0000

    Added addEvents functionality

--- index.js
+++ index.js
@@ -13,15 +13,57 @@ var ERROR = "error";
  * @constructor
  */
 var EchoClient = function(config){
-  this.config = config || {};
+    this.config = config || {};
 
-  var requiredParams = ['echo_host', 'echo_port'];
+    var requiredParams = ['echo_host', 'echo_port'];
 
-  for(var i in requiredParams){
-    if (this.config[requiredParams[i]] === undefined) {
-      throw new Error("Missing Echo config: "+requiredParams[i]);
+    for(var i in requiredParams){
+        if (this.config[requiredParams[i]] === undefined) {
+            throw new Error("Missing Echo config: "+requiredParams[i]);
+        }
     }
-  }
+};
+
+/**
+ * Add an event or events
+ * @todo document what these are
+ * @param {string} token
+ * @param {object} data
+ * @param {string} data.class
+ * @param {string} data.source
+ */
+EchoClient.prototype.addEvents = function(token, data, callback){
+    if(!token){
+        throw new Error('Missing Persona token');
+    }
+    if(!data){
+        throw new Error('Missing data');
+    }
+    if(!data.class){
+        throw new Error('Missing field data.class');
+    }
+    if(!data.source){
+        throw new Error('Missing field data.source');
+    }
+
+    var requestOptions = {
+        url: this.config.echo_host+':'+this.config.echo_port+'/1/events',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization':'Bearer '+token
+        }
+    };
+
+    request.post(requestOptions, function(err, response, body){
+        if(err){
+            callback(err);
+        } else{
+            callback(null, body);
+        }
+    });
+
+    this.debug(JSON.stringify(requestOptions));
+
 };
 
 /**
@@ -33,28 +75,28 @@ var EchoClient = function(config){
  * @private
  */
 EchoClient.prototype._log = function (severity, message) {
-  if (!this.config.enable_debug) {
-    return true;
-  }
-
-  if (this.config.logger) {
-    if (severity === DEBUG) {
-      this.config.logger.debug("[echo_node_client] " + message);
-    } else if (severity === ERROR) {
-      this.config.logger.error("[echo_node_client] " + message);
-    } else {
-      console.log(severity + ": [echo_node_client] " + message);
+    if (!this.config.enable_debug) {
+        return true;
     }
-  } else {
-    console.log(severity + ": [echo_node_client] " + message);
-  }
+
+    if (this.config.logger) {
+        if (severity === DEBUG) {
+            this.config.logger.debug("[echo_node_client] " + message);
+        } else if (severity === ERROR) {
+            this.config.logger.error("[echo_node_client] " + message);
+        } else {
+            console.log(severity + ": [echo_node_client] " + message);
+        }
+    } else {
+        console.log(severity + ": [echo_node_client] " + message);
+    }
 };
 
 EchoClient.prototype.debug = function (message) {
-  this._log(DEBUG, message);
+    this._log(DEBUG, message);
 };
 EchoClient.prototype.error = function (message) {
-  this._log(ERROR, message);
+    this._log(ERROR, message);
 };
 
 /**
@@ -63,5 +105,5 @@ EchoClient.prototype.error = function (message) {
  * @returns {EchoClient}
  */
 exports.createClient = function (config) {
-  return new EchoClient(config);
+    return new EchoClient(config);
 };
--- test/unit/echo_client_test.js
+++ test/unit/echo_client_test.js
@@ -2,34 +2,151 @@
 
 var should = require('should'),
     assert = require('assert'),
-    echo = require('../../index.js');
+    echo = require('../../index.js'),
+    rewire = require('rewire');
 
 describe("Echo Node Client Test Suite", function(){
     describe("- Constructor tests", function(){
-      it("should throw error if config.echo_host is not supplied", function(done){
-        var echoClient = function(){
-          return echo.createClient({});
-        };
-        echoClient.should.throw("Missing Echo config: echo_host");
-        done();
-      });
+        it("- should throw error if config.echo_host is not supplied", function(done){
+            var echoClient = function(){
+                return echo.createClient({});
+            };
+            echoClient.should.throw("Missing Echo config: echo_host");
+            done();
+        });
 
-      it("should throw error if config.echo_port is not supplied", function(done){
-        var echoClient = function(){
-          return echo.createClient({echo_host:'echo'});
-        };
-        echoClient.should.throw("Missing Echo config: echo_port");
-        done();
-      });
-      it("should NOT throw any error if all config params are defined", function(done){
-        var echoClient = function(){
-          return echo.createClient({
-            echo_host:"http://echo",
-            echo_port:3000
-          });
-        };
-        echoClient.should.not.throw();
-        done();
-      });
+        it("- should throw error if config.echo_port is not supplied", function(done){
+            var echoClient = function(){
+                return echo.createClient({echo_host:'echo'});
+            };
+            echoClient.should.throw("Missing Echo config: echo_port");
+            done();
+        });
+        it("- should NOT throw any error if all config params are defined", function(done){
+            var echoClient = function(){
+                return echo.createClient({
+                    echo_host:"http://echo",
+                    echo_port:3000
+            });
+            };
+            echoClient.should.not.throw();
+            done();
+        });
+    });
+
+    describe("- add events test", function(){
+        it("- should throw error if no persona token supplied", function(done){
+            var echoClient = echo.createClient({
+                echo_host:"http://echo",
+                echo_port:3000
+            });
+
+            var addEvents = function(){
+                return echoClient.addEvents(null, null, function(err, result){});
+            };
+
+            addEvents.should.throw("Missing Persona token");
+            done();
+        });
+        it("- should throw error if no data supplied", function(done){
+            var echoClient = echo.createClient({
+                echo_host:"http://echo",
+                echo_port:3000
+            });
+
+            var addEvents = function(){
+                return echoClient.addEvents('secret', null, function(err, result){});
+            };
+
+            addEvents.should.throw("Missing data");
+            done();
+        });
+        it("- should throw error if data.class not supplied", function(done){
+            var echoClient = echo.createClient({
+                echo_host:"http://echo",
+                echo_port:3000
+            });
+
+            var addEvents = function(){
+                return echoClient.addEvents('secret', {}, function(err, result){});
+            };
+
+            addEvents.should.throw("Missing field data.class");
+            done();
+        });
+        it("- should throw error if data.source not supplied", function(done){
+            var echoClient = echo.createClient({
+                echo_host:"http://echo",
+                echo_port:3000
+            });
+
+            var addEvents = function(){
+                return echoClient.addEvents('secret', {class:'test'}, function(err, result){});
+            };
+
+            addEvents.should.throw("Missing field data.source");
+            done();
+        });
+        it("- add events should return an error if call to request returns an error", function(done){
+            var echo = rewire("../../index.js");
+
+            var echoClient = echo.createClient({
+                echo_host:"http://echo",
+                echo_port:3000
+            });
+            var requestStub = {
+                post:function(options, callback){
+                    var error = new Error('Error communicating with Echo');
+                    callback(error);
+                }
+            };
+
+            echo.__set__("request", requestStub);
+
+            echoClient.addEvents('secret', {class:'class', source:'source'}, function(err, result){
+
+                (err === null).should.be.false;
+                err.message.should.equal('Error communicating with Echo');
+                (typeof result).should.equal('undefined');
+            });
+            done();
+        });
+        it("- add events should return no errors if everything is successful", function(done){
+
+            var echo = rewire("../../index.js");
+
+            var echoClient = echo.createClient({
+                echo_host:"http://echo",
+                echo_port:3000
+            });
+
+            var requestMock = {};
+            requestMock.post = function(options, callback){
+                callback(null, {}, {
+                    "class": "page.views",
+                    "timestamp": 1324524509,
+                    "user": "1234-5678-9012-3456",
+                    "source" : "rl-app",
+                    "props": {
+                        "url" : "https://foo.bar/baz.html"
+                    }
+                });
+            };
+
+            echo.__set__("request", requestMock);
+
+            echoClient.addEvents('secret', {class:'class', source:'source'}, function(err, result){
+
+                (err === null).should.be.true;
+
+                result.timestamp.should.equal(1324524509);
+                result.user.should.equal("1234-5678-9012-3456");
+                result.source.should.equal("rl-app");
+                result.props.should.be.an.object;
+                result.props.url.should.equal("https://foo.bar/baz.html");
+
+                done();
+            });
+        });
     });
 });
