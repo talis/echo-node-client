c106c173a533de188fa99821b743412710b9d830
Richard Gubby <rgubby@googlemail.com>
2015-03-02 11:11:04 +0000

    Added echo client constructor

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 'use strict';
+
+var request = require('request');
+
+// log severities
+var DEBUG = "debug";
+var ERROR = "error";
+
+/**
+ * Create an Echo client
+ *
+ * @param {object} config Echo Client config
+ * @constructor
+ */
+var EchoClient = function(config){
+  this.config = config || {};
+
+  var requiredParams = ['echo_host', 'echo_port'];
+
+  for(var i in requiredParams){
+    if (this.config[requiredParams[i]] === undefined) {
+      throw new Error("Missing Echo config: "+requiredParams[i]);
+    }
+  }
+};
+
+/**
+ * Log wrapping functions
+ *
+ * @param severity ( debug or error )
+ * @param message
+ * @returns {boolean}
+ * @private
+ */
+EchoClient.prototype._log = function (severity, message) {
+  if (!this.config.enable_debug) {
+    return true;
+  }
+
+  if (this.config.logger) {
+    if (severity === DEBUG) {
+      this.config.logger.debug("[echo_node_client] " + message);
+    } else if (severity === ERROR) {
+      this.config.logger.error("[echo_node_client] " + message);
+    } else {
+      console.log(severity + ": [echo_node_client] " + message);
+    }
+  } else {
+    console.log(severity + ": [echo_node_client] " + message);
+  }
+};
+
+EchoClient.prototype.debug = function (message) {
+  this._log(DEBUG, message);
+};
+EchoClient.prototype.error = function (message) {
+  this._log(ERROR, message);
+};
+
+/**
+ * The only way to get an instance of the Echo Client is through this method
+ * @param config
+ * @returns {EchoClient}
+ */
+exports.createClient = function (config) {
+  return new EchoClient(config);
+};
--- test/unit/echo_client_test.js
+++ test/unit/echo_client_test.js
@@ -2,10 +2,34 @@
 
 var should = require('should'),
     assert = require('assert'),
-    echo = require('../../index.js'),
-    rewire = require("rewire");
+    echo = require('../../index.js');
 
 describe("Echo Node Client Test Suite", function(){
     describe("- Constructor tests", function(){
+      it("should throw error if config.echo_host is not supplied", function(done){
+        var echoClient = function(){
+          return echo.createClient({});
+        };
+        echoClient.should.throw("Missing Echo config: echo_host");
+        done();
+      });
+
+      it("should throw error if config.echo_port is not supplied", function(done){
+        var echoClient = function(){
+          return echo.createClient({echo_host:'echo'});
+        };
+        echoClient.should.throw("Missing Echo config: echo_port");
+        done();
+      });
+      it("should NOT throw any error if all config params are defined", function(done){
+        var echoClient = function(){
+          return echo.createClient({
+            echo_host:"http://echo",
+            echo_port:3000
+          });
+        };
+        echoClient.should.not.throw();
+        done();
+      });
     });
 });
